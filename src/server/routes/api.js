d758363ac1ba27e9827a423b6d8b8276f868b681
Amelie Oller <amelieoller@gmail.com>
2018-11-05 20:15:57 -0800

    Create user api route

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import moment from "moment";
 
 const api = db => {
   const router = Router();
   const boards = db.collection("boards");
+  const users = db.collection("users");
 
   // Replace the entire board every time the users modifies it in any way.
   // This solution sends more data than necessary, but cuts down on code and
@@ -23,6 +25,24 @@ const api = db => {
     boards.deleteOne({ _id: boardId }).then(result => {
       res.send(result);
     });
+  });
+
+  router.put("/user", (req, res) => {
+    const user = req.body;
+
+    users
+      .updateOne(
+        { _id: user._id },
+        {
+          $set: {
+            accessToken: user.accessToken,
+            expiryDate: user.expiryDate
+          }
+        }
+      )
+      .then(result => {
+        res.send(result);
+      });
   });
 
   return router;
