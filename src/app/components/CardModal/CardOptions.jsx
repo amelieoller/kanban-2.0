fd6875b1efaa7dd7b817da8614d9ea241f01fd7b
Amelie Oller <amelieoller@gmail.com>
2018-12-19 07:05:39 -0800

    Refactor modal styles

--- src/app/components/CardModal/CardOptions.jsx
+++ src/app/components/CardModal/CardOptions.jsx
@@ -37,6 +37,58 @@ const CardOptionsStyles = styled.div`
     font-size: inherit;
     cursor: pointer;
   }
+
+  .calendar-modal {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+  }
+
+  .calendar-underlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 3;
+  }
+
+  .calendar {
+    display: flex;
+    flex-direction: column;
+    border-radius: 3px;
+    background: white;
+  }
+
+  .calendar-buttons {
+    display: flex;
+    justify-content: space-around;
+    margin-bottom: 8px;
+
+    button {
+      width: 120px;
+      height: 30px;
+      border: 0;
+      border-radius: 3px;
+      font-size: 14px;
+      font-weight: 700;
+      transition: background 0.2s;
+      cursor: pointer;
+    }
+
+    button:hover {
+      background: #bbb;
+    }
+
+    .calendar-save-button {
+      color: white;
+      background: $success-button-color;
+    }
+
+    .calendar-save-button:hover {
+      background: $success-button-color-hover;
+    }
+  }
 `;
 
 class CardOptions extends Component {
@@ -106,13 +158,13 @@ class CardOptions extends Component {
     if (card.category !== category) {
       if (category.color === "white") {
         dispatch({
-					type: "DELETE_CARD_CATEGORY",
+          type: "DELETE_CARD_CATEGORY",
           payload: { cardId: card._id }
         });
       } else {
         dispatch({
           type: "CHANGE_CARD_CATEGORY",
-					payload: { categoryId: category._id, cardId: card._id }
+          payload: { categoryId: category._id, cardId: card._id }
         });
       }
     }
