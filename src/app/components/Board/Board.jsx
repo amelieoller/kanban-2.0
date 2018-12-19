5bc795eacc9a63749770a0721bafc6e6f9bfe7cf
Amelie Oller <amelieoller@gmail.com>
2018-12-19 06:57:49 -0800

    Move board styles to use styled components

--- src/app/components/Board/Board.jsx
+++ src/app/components/Board/Board.jsx
@@ -3,14 +3,44 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Title } from "react-head";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
-import classnames from "classnames";
 import List from "../List/List";
 import ListAdder from "../ListAdder/ListAdder";
 import Header from "../Header/Header";
 import Sidebar from "../Sidebar/Sidebar";
 import SidebarRight from "../SidebarRight/SidebarRight";
-import "./Board.scss";
 import Habits from "../Habits/Habits";
+import styled from "styled-components";
+
+const BoardStyles = styled.div`
+  display: inline-flex;
+  height: 100%;
+  min-width: 100%;
+
+  .board-underlay {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: -1;
+    transition: background 0.3s;
+    background: ${props => props.theme.mainBackground};
+  }
+
+  .lists-wrapper {
+    display: inline-flex;
+    box-sizing: border-box;
+    height: 100%;
+    padding: 60px 5px 8px 5px;
+  }
+
+  .lists {
+    display: inline-flex;
+    align-items: flex-start;
+    height: 100%;
+    user-select: none;
+  }
+`;
 
 class Board extends Component {
   static propTypes = {
@@ -155,53 +185,53 @@ class Board extends Component {
       boardColor,
       pomodoro,
       completedListId,
-      habitsListId
+      habitsListId,
+      changeTheme,
+      setBoardColor
     } = this.props;
     const otherLists = lists.filter(
       list => list && list._id !== completedListId && list._id !== habitsListId
     );
 
     return (
-      <>
-        <div className={classnames("board", boardColor)}>
-          <Title>{boardTitle} | Kanban 2.0</Title>
-          <Header />
-          {/* eslint-disable jsx-a11y/no-static-element-interactions */}
-          <Sidebar pomodoro={pomodoro} boardId={boardId} />
-          <Habits boardId={boardId} />
-          <div
-            className="lists-wrapper"
-            onMouseDown={this.handleMouseDown}
-            onWheel={this.handleWheel}
-          >
-            {/* eslint-enable jsx-a11y/no-static-element-interactions */}
-            <DragDropContext onDragEnd={this.handleDragEnd}>
-              <Droppable
-                droppableId={boardId}
-                type="COLUMN"
-                direction="horizontal"
-              >
-                {provided => (
-                  <div className="lists" ref={provided.innerRef}>
-                    {otherLists.map((list, index) => (
-                      <List
-                        list={list}
-                        boardId={boardId}
-                        index={index}
-                        key={list._id}
-                      />
-                    ))}
-                    {provided.placeholder}
-                    <ListAdder boardId={boardId} />
-                  </div>
-                )}
-              </Droppable>
-            </DragDropContext>
-          </div>
-          <SidebarRight pomodoro={pomodoro} boardId={boardId} />
-          <div className="board-underlay" />
+      <BoardStyles>
+        <Title>{boardTitle} | Kanban 2.0</Title>
+        <Header changeTheme={changeTheme} setBoardColor={setBoardColor} />
+        {/* eslint-disable jsx-a11y/no-static-element-interactions */}
+        <Sidebar pomodoro={pomodoro} boardId={boardId} />
+        <Habits boardId={boardId} />
+        <div
+          className="lists-wrapper"
+          onMouseDown={this.handleMouseDown}
+          onWheel={this.handleWheel}
+        >
+          {/* eslint-enable jsx-a11y/no-static-element-interactions */}
+          <DragDropContext onDragEnd={this.handleDragEnd}>
+            <Droppable
+              droppableId={boardId}
+              type="COLUMN"
+              direction="horizontal"
+            >
+              {provided => (
+                <div className="lists" ref={provided.innerRef}>
+                  {otherLists.map((list, index) => (
+                    <List
+                      list={list}
+                      boardId={boardId}
+                      index={index}
+                      key={list._id}
+                    />
+                  ))}
+                  {provided.placeholder}
+                  <ListAdder boardId={boardId} />
+                </div>
+              )}
+            </Droppable>
+          </DragDropContext>
         </div>
-      </>
+        <SidebarRight pomodoro={pomodoro} boardId={boardId} />
+        <div className="board-underlay" />
+      </BoardStyles>
     );
   };
 }
