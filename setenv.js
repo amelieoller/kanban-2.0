e9cdd5ad970a85c3a61ced3ce094f8e0a005ea8f
Amelie Oller <amelieoller@gmail.com>
2019-01-01 17:06:45 -0800

    Update package.json and env console log

--- setenv.js
+++ setenv.js
@@ -1,6 +1,6 @@
 const env = process.env.NODE_ENV;
-if (env === 'production') {
-	console.log(`API_URL=${process.env.API_URL}`);
+if (env === "production") {
+  console.log(`API_URL=${process.env.API_URL}`);
 } else {
-	console.log(`API_URL=http://localhost:8000`);
+  console.log(`API_URL=http://127.0.0.1:1337`);
 }
