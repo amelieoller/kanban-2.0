5cc99329b62ec6cc4b0d8ed25ad3826aecaa3c4f
Amelie Oller <amelieoller@gmail.com>
2019-04-27 00:13:55 -0700

    Update uglify options

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -1,13 +1,13 @@
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
-const ManifestPlugin = require("webpack-manifest-plugin");
-const CleanPlugin = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const ZopfliPlugin = require("zopfli-webpack-plugin");
-const UglifyPlugin = require("uglifyjs-webpack-plugin");
-const autoprefixer = require("autoprefixer");
-const webpack = require("webpack");
-const dotenv = require("dotenv");
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ZopfliPlugin = require('zopfli-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 
 const env = dotenv.config().parsed;
 
@@ -17,95 +17,97 @@ const envKeys = Object.keys(env).reduce((prev, next) => {
 }, {});
 
 module.exports = {
-  name: "client",
-  target: "web",
-  entry: "./src/client.jsx",
-  output: {
-    path: path.join(__dirname, "dist/public"),
-    publicPath: "/static/",
-    filename: "bundle.[hash:6].js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: {
-          loader: "babel-loader"
-        },
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(css|scss)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1
-            }
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              ident: "postcss",
-              plugins: [autoprefixer()]
-            }
-          },
-          {
-            loader: "sass-loader"
-          }
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf)$/,
-        loader: "url-loader?limit=100000"
-      },
-      {
-        test: /\.(png|jpg|gif|mp3)$/,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              limit: 4096,
-              name: "[name].[hash:6].[ext]",
-              outputPath: "images/"
-            }
-          }
-        ]
-      },
-      {
-        test: /\.svg$/,
-        use: {
-          loader: "svg-url-loader",
-          options: {
-            noquotes: true,
-            limit: 4096,
-            name: "[name].[hash:6].[ext]",
-            outputPath: "images/"
-          }
-        }
-      }
-    ]
-  },
-  plugins: [
+	name: 'client',
+	target: 'web',
+	entry: './src/client.jsx',
+	output: {
+		path: path.join(__dirname, 'dist/public'),
+		publicPath: '/static/',
+		filename: 'bundle.[hash:6].js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				use: {
+					loader: 'babel-loader'
+				},
+				exclude: /node_modules/
+			},
+			{
+				test: /\.(css|scss)$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 1
+						}
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							ident: 'postcss',
+							plugins: [autoprefixer()]
+						}
+					},
+					{
+						loader: 'sass-loader'
+					}
+				]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf)$/,
+				loader: 'url-loader?limit=100000'
+			},
+			{
+				test: /\.(png|jpg|gif|mp3)$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 4096,
+							name: '[name].[hash:6].[ext]',
+							outputPath: 'images/'
+						}
+					}
+				]
+			},
+			{
+				test: /\.svg$/,
+				use: {
+					loader: 'svg-url-loader',
+					options: {
+						noquotes: true,
+						limit: 4096,
+						name: '[name].[hash:6].[ext]',
+						outputPath: 'images/'
+					}
+				}
+			}
+		]
+	},
+	plugins: [
 		new webpack.DefinePlugin(envKeys),
-    new CleanPlugin(["dist"]),
-    new CopyPlugin([{ from: "src/assets/favicons", to: "favicons" }]),
-    new ManifestPlugin(),
-    new MiniCssExtractPlugin(),
-    new ZopfliPlugin(),
-    new UglifyPlugin({
-      uglifyOptions: {
-        output: {
-          comments: false
-        },
-        compress: {
-          drop_console: true
-        }
-      }
-    })
-  ],
-  resolve: {
-    extensions: [".js", ".jsx"]
-  }
+		new CleanPlugin(['dist']),
+		new CopyPlugin([{ from: 'src/assets/favicons', to: 'favicons' }]),
+		new ManifestPlugin(),
+		new MiniCssExtractPlugin(),
+		new ZopfliPlugin(),
+		new webpack.DefinePlugin({
+			PRODUCTION: JSON.stringify(true)
+		}),
+		new UglifyJsPlugin({
+			sourceMap: true,
+			uglifyOptions: {
+				ecma: 8,
+				compress: {
+					warnings: false
+				}
+			}
+		})
+	],
+	resolve: {
+		extensions: ['.js', '.jsx']
+	}
 };
