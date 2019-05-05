006ebb95f6ad1bee826ad8be78da61ee0d3902ae
Amelie Oller <amelieoller@gmail.com>
2019-05-05 10:12:37 -0700

    Remove unused code from stats and make functional

--- src/app/components/Habits/HabitStats.jsx
+++ src/app/components/Habits/HabitStats.jsx
@@ -1,10 +1,9 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import moment from "moment";
-import { FiStar } from "react-icons/fi";
-import styled from "styled-components";
-import ChartistGraph from "react-chartist";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import moment from 'moment';
+import styled from 'styled-components';
+import ChartistGraph from 'react-chartist';
 
 const GraphStyles = styled.div`
   .ct-series-a .ct-line {
@@ -34,7 +33,7 @@ const GraphStyles = styled.div`
     white-space: nowrap;
     font-size: 12px;
     color: ${props => props.theme.colors.grey};
-    font-family: "Pacifico", cursive;
+    font-family: 'Pacifico', cursive;
     margin-top: 3px;
   }
 `;
@@ -60,8 +59,8 @@ const ProgressBarStyles = styled.div`
         background: transparent;
       }
 
-      input[type="number"]::-webkit-inner-spin-button,
-      input[type="number"]::-webkit-outer-spin-button {
+      input[type='number']::-webkit-inner-spin-button,
+      input[type='number']::-webkit-outer-spin-button {
         -webkit-appearance: none;
         -moz-appearance: none;
         appearance: none;
@@ -85,45 +84,15 @@ const ProgressBarStyles = styled.div`
   }
 `;
 
-class HabitStats extends Component {
-  static propTypes = {};
-
-  constructor(props) {
-    super(props);
-    const today = new Date();
-    const date = `${today.getFullYear()}-${today.getMonth() +
-      1}-${today.getDate()}`;
-    const habits =
-      this.props.stats.habits[date] && this.props.stats.habits[date].length;
-
-    this.state = {
-      number: habits || 0
-    };
-  }
-
-  switchClass = () => {
-    const { number } = this.state;
-    this.setState({
-      number: number + 1,
-      className: "number numberLarge"
-    });
-
-    setTimeout(() => {
-      this.setState({
-        className: "number"
-      });
-    }, 1200);
-  };
-
-  calculateLabel = num => {
+const HabitStats = ({ stats, cards, dispatch, boardId, habitGoals }) => {
+  const calculateLabel = num => {
     const today = new Date();
     today.setDate(today.getDate() - num);
 
-		return moment(today).format("ddd");
+    return moment(today).format('ddd');
   };
 
-  calculateSeries = num => {
-    const { stats, cards } = this.props;
+  const calculateSeries = num => {
     let result = 0;
     const today = new Date();
     today.setDate(today.getDate() - num);
@@ -142,93 +111,92 @@ class HabitStats extends Component {
     return result;
   };
 
-  handleSettingsChange = (type, value) => {
-    const { dispatch, boardId } = this.props;
-
+  const handleSettingsChange = (type, value) => {
     dispatch({
-      type: "CHANGE_GOAL_SETTING",
+      type: 'CHANGE_GOAL_SETTING',
       payload: { boardId, type, value }
     });
   };
 
-  render = () => {
-    const { habitGoals } = this.props;
+  const calculateWidth = calculateSeries(0) / parseInt(habitGoals, 10);
+  const progressWidth =
+    calculateWidth > 1 ? `100%` : `${calculateWidth * 100}%`;
 
-    const calculateWidth = this.calculateSeries(0) / parseInt(habitGoals);
-    const progressWidth =
-      calculateWidth > 1 ? `100%` : `${calculateWidth * 100}%`;
-
-    const data = {
-      labels: [
-        this.calculateLabel(4),
-        this.calculateLabel(3),
-        this.calculateLabel(2),
-        this.calculateLabel(1),
-        this.calculateLabel(0)
-      ],
-      series: [
-        [
-          this.calculateSeries(4),
-          this.calculateSeries(3),
-          this.calculateSeries(2),
-          this.calculateSeries(1),
-          this.calculateSeries(0)
-        ]
+  const data = {
+    labels: [
+      calculateLabel(4),
+      calculateLabel(3),
+      calculateLabel(2),
+      calculateLabel(1),
+      calculateLabel(0)
+    ],
+    series: [
+      [
+        calculateSeries(4),
+        calculateSeries(3),
+        calculateSeries(2),
+        calculateSeries(1),
+        calculateSeries(0)
       ]
-    };
-
-    const options = {
-      low: 0,
-      showArea: true,
-      fullWidth: true,
-      chartPadding: 12,
-      axisX: {
-        showGrid: false
-      },
-      axisY: {
-        offset: 0,
-        showLabel: false,
-        showGrid: true
-      },
-      width: "100%",
-      height: "140px"
-    };
-
-    return (
-      <>
-        <ProgressBarStyles>
-          <div className="header">
-            Habits · <span className="number">{this.calculateSeries(0)}</span>
-          </div>
-          <hr />
-          <div className="habit-target">
-            <div className="habit-progress-wrapper">
-              <div
-                className="habit-progress"
-                style={{
-                  width: progressWidth
-                }}
-              />
-            </div>
-            <span className="goal">
-              <FiStar />
-              <input
-                type="number"
-                onChange={e =>
-                  this.handleSettingsChange("habits", e.target.value)
-                }
-                value={habitGoals}
-              />
-            </span>
-          </div>
-        </ProgressBarStyles>
-        <GraphStyles>
-          <ChartistGraph data={data} options={options} type="Line" />
-        </GraphStyles>
-      </>
-    );
+    ]
   };
-}
+
+  const options = {
+    low: 0,
+    showArea: true,
+    fullWidth: true,
+    chartPadding: 12,
+    axisX: {
+      showGrid: false
+    },
+    axisY: {
+      offset: 0,
+      showLabel: false,
+      showGrid: true
+    },
+    width: '100%',
+    height: '140px'
+  };
+
+  return (
+    <>
+      <ProgressBarStyles>
+        <div className="header">
+          Habits · <span className="number">{calculateSeries(0)}</span>
+        </div>
+        <hr />
+        <div className="habit-target">
+          <div className="habit-progress-wrapper">
+            <div
+              className="habit-progress"
+              style={{
+                width: progressWidth
+              }}
+            />
+          </div>
+          <span className="goal">
+            <input
+              type="number"
+              onChange={e => handleSettingsChange('habits', e.target.value)}
+              value={habitGoals}
+            />
+          </span>
+        </div>
+      </ProgressBarStyles>
+      <GraphStyles>
+        <ChartistGraph data={data} options={options} type="Line" />
+      </GraphStyles>
+    </>
+  );
+};
+
+HabitStats.propTypes = {
+  stats: PropTypes.object.isRequired,
+  cards: PropTypes.object.isRequired,
+  dispatch: PropTypes.func.isRequired,
+  boardId: PropTypes.string.isRequired,
+  habitGoals: PropTypes.string.isRequired
+};
 
 const mapStateToProps = (state, ownProps) => ({
   stats: state.boardsById[ownProps.boardId].stats,
