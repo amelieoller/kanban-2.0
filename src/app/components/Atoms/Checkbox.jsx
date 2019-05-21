8a5b6331f7f73d11df44f162a0f315bca502cc99
Amelie Oller <amelieoller@gmail.com>
2019-05-20 17:10:31 -0700

    Update checkbox component

--- src/app/components/Atoms/Checkbox.jsx
+++ src/app/components/Atoms/Checkbox.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { FiSquare, FiCheckSquare } from 'react-icons/fi';
+import { useTransition, animated } from 'react-spring';
 
 const StyledCheckbox = styled.div`
   display: flex;
   align-items: center;
+  cursor: pointer;
 
   label {
     margin-left: 3px;
@@ -18,25 +20,62 @@ const StyledCheckbox = styled.div`
   svg.selected {
     color: ${props => props.theme.colors[props.color]};
   }
+
+  .check {
+    width: 16px;
+    position: relative;
+
+    div {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: absolute;
+      will-change: opacity;
+    }
+  }
 `;
 
-const Checkbox = ({ label, onChange, checked, name, color }) => {
-  const [isChecked, setIsChecked] = useState(checked);
+const boxes = [
+  ({ style }) => (
+    <animated.div style={{ ...style }}>
+      <FiSquare />
+    </animated.div>
+  ),
+  ({ style }) => (
+    <animated.div style={{ ...style }}>
+      <FiCheckSquare className="selected" />
+    </animated.div>
+  )
+];
 
-  const handleCheck = e => {
-    onChange(e, !checked, 'pomodoroFocusMode');
-    setIsChecked(!checked);
+const Checkbox = ({ label, onChange, checked, name, color }) => {
+  const [isChecked, setIsChecked] = useState(checked ? 1 : 0);
+
+  const onClick = useCallback(() => setIsChecked(state => (state + 1) % 2), []);
+
+  const handleCheck = () => {
+    onChange();
+    onClick();
   };
 
+  const transitions = useTransition(isChecked, p => p, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 }
+  });
+
   return (
-    <StyledCheckbox className="checkbox" color={color}>
-      {isChecked ? (
-        <FiCheckSquare onClick={handleCheck} name={name} className="selected" />
-      ) : (
-        <FiSquare onClick={handleCheck} name={name} />
-      )}
+    <StyledCheckbox className="checkbox" color={color} onClick={handleCheck}>
+      <div className="check">
+        {transitions.map(({ item, props, key }) => {
+          const Page = boxes[item];
+          return <Page key={key} style={props} />;
+        })}
+      </div>
       {label && (
-        <label htmlFor="checkbox" onClick={handleCheck}>
+        <label htmlFor="checkbox" name={name}>
           {label}
         </label>
       )}
@@ -45,15 +84,26 @@ const Checkbox = ({ label, onChange, checked, name, color }) => {
 };
 
 Checkbox.defaultProps = {
-  color: 'secondary'
+  color: 'secondary',
+  checked: false
 };
 
 Checkbox.propTypes = {
   label: PropTypes.string,
   onChange: PropTypes.func.isRequired,
-  checked: PropTypes.bool.isRequired,
+  checked: PropTypes.bool,
   name: PropTypes.string.isRequired,
   color: PropTypes.string
 };
 
 export default Checkbox;
+
+// {isChecked ? (
+//   <FiCheckSquare
+//     onClick={handleCheck}
+//     name={name}
+//     className="selected"
+//   />
+// ) : (
+//   <FiSquare onClick={handleCheck} name={name} />
+// )}
