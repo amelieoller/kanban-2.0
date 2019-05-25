6cbad3e3879a32c3063af428bc9d76f372c27fcb
Amelie Oller <amelieoller@gmail.com>
2019-05-25 12:39:12 -0700

    Add babel-plugin-styled-components

--- babel.config.js
+++ babel.config.js
@@ -10,5 +10,13 @@ module.exports = {
     ],
     '@babel/preset-react'
   ],
-  plugins: ['@babel/plugin-proposal-class-properties']
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    [
+      'babel-plugin-styled-components',
+      {
+        pure: true
+      }
+    ]
+  ]
 };
