dfaf06e26beee0c3ab1c718a42785fb8517dfa20
Amelie Oller <amelieoller@gmail.com>
2019-07-02 19:18:39 -0700

    Move event and task no info divs into components

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -10,210 +10,187 @@ import Habits from './Habits/Habits';
 import ExpandablePanels from './Molecules/ExpandablePanels';
 
 const SidebarStyles = styled.div`
-  height: 100%;
-  min-height: 100vh;
-  position: fixed;
-  top: 0;
-  left: 0;
-  margin-top: ${props => `${props.theme.sizes.headerHeight}px`};
-  overflow: scroll;
-  box-shadow: ${props => props.theme.common.boxShadowTwo};
-  background: ${props => props.theme.colors.elevatedThree};
-  display: grid;
-  grid-template-columns: ${props => `${props.theme.sizes.sidebarWidth}px`};
-  grid-auto-rows: max-content;
-  z-index: 1;
+	height: 100%;
+	min-height: 100vh;
+	position: fixed;
+	top: 0;
+	left: 0;
+	margin-top: ${props => `${props.theme.sizes.headerHeight}px`};
+	overflow: scroll;
+	box-shadow: ${props => props.theme.common.boxShadowTwo};
+	background: ${props => props.theme.colors.elevatedThree};
+	display: grid;
+	grid-template-columns: ${props => `${props.theme.sizes.sidebarWidth}px`};
+	grid-auto-rows: max-content;
+	z-index: 1;
 
-  .no-items {
-    color: ${props => props.theme.colors.text};
-    font-size: 0.8rem;
-    font-style: italic;
-    margin: 0;
-    padding: 10px;
-  }
+	.no-items {
+		color: ${props => props.theme.colors.text};
+		font-size: 0.8rem;
+		font-style: italic;
+		margin: 0;
+		padding: 10px;
+	}
 
-  .open-settings-link {
-    text-decoration: underline;
-    cursor: pointer;
-    color: ${props => props.theme.colors.secondary};
+	.open-settings-link {
+		text-decoration: underline;
+		cursor: pointer;
+		color: ${props => props.theme.colors.secondary};
 
-    &:hover {
-      color: ${props => props.theme.colors.secondaryDark};
-    }
-  }
+		&:hover {
+			color: ${props => props.theme.colors.secondaryDark};
+		}
+	}
 
-  @media ${props => props.theme.media.tablet} {
-    margin-top: ${props => `${props.theme.sizes.headerHeightMobile}px`};
-  }
+	@media ${props => props.theme.media.tablet} {
+		margin-top: ${props => `${props.theme.sizes.headerHeightMobile}px`};
+	}
 
-  @media ${props => props.theme.media.phone} {
-    display: ${props => (props.isKeyboardOpen ? 'none' : 'grid')};
-    grid-template-columns: 100%;
-    width: 100vw;
-    min-height: auto;
-    height: ${props => `${props.theme.sizes.footerHeight}px`};
-    bottom: 0;
-    top: auto;
-  }
+	@media ${props => props.theme.media.phone} {
+		display: ${props => (props.isKeyboardOpen ? 'none' : 'grid')};
+		grid-template-columns: 100%;
+		width: 100vw;
+		min-height: auto;
+		height: ${props => `${props.theme.sizes.footerHeight}px`};
+		bottom: 0;
+		top: auto;
+	}
 
-  & > div {
-    padding: 10px;
-    width: 100%;
-    height: 100%;
+	& > div {
+		padding: 10px;
+		width: 100%;
+		height: 100%;
 
-    @media ${props => props.theme.media.tablet} {
-      &:nth-child(odd) {
-        border-right: 1px solid ${props => props.theme.colors.textDisabled};
-      }
-    }
-  }
+		@media ${props => props.theme.media.tablet} {
+			&:nth-child(odd) {
+				border-right: 1px solid ${props => props.theme.colors.textDisabled};
+			}
+		}
+	}
 `;
 
 const Sidebar = ({
-  cards,
-  pomodoro,
-  dispatch,
-  boardId,
-  user,
-  completedListId,
-  categories,
-  eventCalendarId,
-  eventFilter,
-  pomodoroFocusMode,
-  isKeyboardOpen,
-  sidebarOpen
+	cards,
+	pomodoro,
+	dispatch,
+	boardId,
+	user,
+	completedListId,
+	categories,
+	eventCalendarId,
+	eventFilter,
+	pomodoroFocusMode,
+	isKeyboardOpen,
+	sidebarOpen
 }) => {
-  const [pomodoriToEvent, setPomodoriToEvent] = useState(false);
+	const [pomodoriToEvent, setPomodoriToEvent] = useState(false);
 
-  const toggleSettingsMenu = () => {
-    dispatch({
-      type: 'TOGGLE_SETTINGS_MENU',
-      payload: { settingsMenuOpen: true }
-    });
-  };
+	const toggleSettingsMenu = () => {
+		dispatch({
+			type: 'TOGGLE_SETTINGS_MENU',
+			payload: { settingsMenuOpen: true }
+		});
+	};
 
-  return (
-    <SidebarStyles isKeyboardOpen={isKeyboardOpen}>
-      <ExpandablePanels>
-        <PomodoroWrapper
-          pomodoro={pomodoro}
-          dispatch={dispatch}
-          boardId={boardId}
-          pomodoriToEvent={pomodoriToEvent}
-          pomodoroFocusMode={pomodoroFocusMode}
-          name="Pomodoro"
-          defaultOpen={sidebarOpen.pomodoroOpen}
-        />
+	return (
+		<SidebarStyles isKeyboardOpen={isKeyboardOpen}>
+			<ExpandablePanels>
+				<PomodoroWrapper
+					pomodoro={pomodoro}
+					dispatch={dispatch}
+					boardId={boardId}
+					pomodoriToEvent={pomodoriToEvent}
+					pomodoroFocusMode={pomodoroFocusMode}
+					name="Pomodoro"
+					defaultOpen={sidebarOpen.pomodoroOpen}
+				/>
 
-        {user && eventCalendarId ? (
-          <Events
-            user={user}
-            dispatch={dispatch}
-            eventCalendarId={eventCalendarId}
-            eventFilter={eventFilter}
-            setPomodoriToEvent={setPomodoriToEvent}
-            name="Events"
-            defaultOpen={sidebarOpen.eventsOpen}
-            boardId={boardId}
-          />
-        ) : (
-          <p className="no-items" name="Events">
-            To see your upcoming events, make sure you are signed in and have
-            added a Calendar ID in the{' '}
-            <span
-              role="link"
-              className="open-settings-link"
-              onClick={toggleSettingsMenu}
-              onKeyDown={toggleSettingsMenu}
-              tabIndex={0}
-            >
-              settings panel
-            </span>
-            .
-          </p>
-        )}
+				<Events
+					user={user}
+					dispatch={dispatch}
+					eventCalendarId={eventCalendarId}
+					eventFilter={eventFilter}
+					setPomodoriToEvent={setPomodoriToEvent}
+					name="Events"
+					defaultOpen={sidebarOpen.eventsOpen}
+					boardId={boardId}
+				/>
 
-        <Habits
-          boardId={boardId}
-          dispatch={dispatch}
-          name="Habits"
-          defaultOpen={sidebarOpen.habitsOpen}
-        />
+				<Habits
+					boardId={boardId}
+					dispatch={dispatch}
+					name="Habits"
+					defaultOpen={sidebarOpen.habitsOpen}
+				/>
 
-        {cards.length !== 0 ? (
-          <TaskStats
-            cards={cards}
-            completedListId={completedListId}
-            dispatch={dispatch}
-            categories={categories}
-            name="Tasks"
-            defaultOpen={sidebarOpen.tasksOpen}
-            boardId={boardId}
-          />
-        ) : (
-          <p className="no-items" name="Tasks">
-            You don't have any completed tasks at this moment.
-          </p>
-        )}
-      </ExpandablePanels>
+				<TaskStats
+					cards={cards}
+					completedListId={completedListId}
+					dispatch={dispatch}
+					categories={categories}
+					name="Tasks"
+					defaultOpen={sidebarOpen.tasksOpen}
+					boardId={boardId}
+				/>
+			</ExpandablePanels>
 
-      {/* <RepeatingTasks pomodoro={pomodoro} boardId={boardId} /> */}
-    </SidebarStyles>
-  );
+			{/* <RepeatingTasks pomodoro={pomodoro} boardId={boardId} /> */}
+		</SidebarStyles>
+	);
 };
 
 Sidebar.propTypes = {
-  cards: PropTypes.arrayOf(
-    PropTypes.shape({
-      _id: PropTypes.string.isRequired,
-      text: PropTypes.string.isRequired,
-      difficulty: PropTypes.number.isRequired
-    }).isRequired
-  ),
-  pomodoro: PropTypes.object.isRequired,
-  dispatch: PropTypes.func.isRequired,
-  boardId: PropTypes.string.isRequired,
-  completedListId: PropTypes.string.isRequired,
-  user: PropTypes.object,
-  categories: PropTypes.array,
-  eventCalendarId: PropTypes.string,
-  eventFilter: PropTypes.string,
-  pomodoroFocusMode: PropTypes.bool,
-  isKeyboardOpen: PropTypes.bool,
-  sidebarOpen: PropTypes.object
+	cards: PropTypes.arrayOf(
+		PropTypes.shape({
+			_id: PropTypes.string.isRequired,
+			text: PropTypes.string.isRequired,
+			difficulty: PropTypes.number.isRequired
+		}).isRequired
+	),
+	pomodoro: PropTypes.object.isRequired,
+	dispatch: PropTypes.func.isRequired,
+	boardId: PropTypes.string.isRequired,
+	completedListId: PropTypes.string.isRequired,
+	user: PropTypes.object,
+	categories: PropTypes.array,
+	eventCalendarId: PropTypes.string,
+	eventFilter: PropTypes.string,
+	pomodoroFocusMode: PropTypes.bool,
+	isKeyboardOpen: PropTypes.bool,
+	sidebarOpen: PropTypes.object
 };
 
 const mapStateToProps = (state, ownProps) => {
-  const { completedListId } = state.boardsById[ownProps.boardId].settings;
-  const {
-    user,
-    boardsById: {
-      [ownProps.boardId]: {
-        settings: {
-          categories,
-          eventCalendarId,
-          eventFilter,
-          pomodoroFocusMode,
-          sidebarOpen
-        }
-      }
-    }
-  } = state;
-  const { isKeyboardOpen } = state.appState;
+	const { completedListId } = state.boardsById[ownProps.boardId].settings;
+	const {
+		user,
+		boardsById: {
+			[ownProps.boardId]: {
+				settings: {
+					categories,
+					eventCalendarId,
+					eventFilter,
+					pomodoroFocusMode,
+					sidebarOpen
+				}
+			}
+		}
+	} = state;
+	const { isKeyboardOpen } = state.appState;
 
-  return {
-    cards: state.listsById[completedListId].cards.map(
-      cardId => state.cardsById[cardId]
-    ),
-    completedListId,
-    user,
-    categories,
-    eventCalendarId,
-    eventFilter,
-    pomodoroFocusMode,
-    isKeyboardOpen,
-    sidebarOpen
-  };
+	return {
+		cards: state.listsById[completedListId].cards.map(
+			cardId => state.cardsById[cardId]
+		),
+		completedListId,
+		user,
+		categories,
+		eventCalendarId,
+		eventFilter,
+		pomodoroFocusMode,
+		isKeyboardOpen,
+		sidebarOpen
+	};
 };
 
 export default connect(mapStateToProps)(Sidebar);
--- src/app/components/TaskStats.jsx
+++ src/app/components/TaskStats.jsx
@@ -7,225 +7,229 @@ import formatMarkdown from './Card/formatMarkdown';
 import IconButton from './Atoms/IconButton';
 
 const TaskStatsStyled = styled.div`
-  ul {
-    margin: 0;
-  }
+	ul {
+		margin: 0;
+	}
 
-  h4 {
-    margin-bottom: 5px;
-    margin-top: 10px;
-    color: ${props => props.theme.colors.secondary};
-  }
+	h4 {
+		margin-bottom: 5px;
+		margin-top: 10px;
+		color: ${props => props.theme.colors.secondary};
+	}
 
-  .completed-task-wrapper {
-    color: ${props => props.theme.colors.text};
-    width: 100%;
-    margin: 0;
-    padding: 5px 0px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    position: relative;
+	.completed-task-wrapper {
+		color: ${props => props.theme.colors.text};
+		width: 100%;
+		margin: 0;
+		padding: 5px 0px;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		position: relative;
 
-    &:not(:last-child) {
-      border-bottom: 1px solid ${props => props.theme.colors.textDisabled};
-    }
+		&:not(:last-child) {
+			border-bottom: 1px solid ${props => props.theme.colors.textDisabled};
+		}
 
-    &:hover,
-    &:focus {
-      background-color: ${props => props.theme.colors.elevatedOne};
+		&:hover,
+		&:focus {
+			background-color: ${props => props.theme.colors.elevatedOne};
 
-      .card-icon {
-        visibility: visible;
-        opacity: 0.8;
-        transition: visibility 0s linear 0s, opacity 200ms;
-      }
-    }
+			.card-icon {
+				visibility: visible;
+				opacity: 0.8;
+				transition: visibility 0s linear 0s, opacity 200ms;
+			}
+		}
 
-    .completed-task-text {
-      font-size: 15px;
-      width: 100%;
-      margin: 0;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      color: ${props => props.theme.colors.textSecondary};
+		.completed-task-text {
+			font-size: 15px;
+			width: 100%;
+			margin: 0;
+			overflow: hidden;
+			text-overflow: ellipsis;
+			color: ${props => props.theme.colors.textSecondary};
 
-      p {
-        margin: 0;
-      }
-    }
+			p {
+				margin: 0;
+			}
+		}
 
-    .card-icon {
-      opacity: 0.7;
-      padding: 2px;
-      position: absolute;
-      right: -2px;
-      top: 0;
-      visibility: hidden;
-      transition: visibility 0s linear 20ms, opacity 20ms;
-      cursor: pointer;
-      border-radius: ${props => props.theme.sizes.borderRadius};
-      background: ${props => props.theme.colors.elevatedOne};
+		.card-icon {
+			opacity: 0.7;
+			padding: 2px;
+			position: absolute;
+			right: -2px;
+			top: 0;
+			visibility: hidden;
+			transition: visibility 0s linear 20ms, opacity 20ms;
+			cursor: pointer;
+			border-radius: ${props => props.theme.sizes.borderRadius};
+			background: ${props => props.theme.colors.elevatedOne};
 
-      &:hover,
-      &:focus {
-        color: ${props => props.theme.colors.primary};
-      }
-    }
-  }
+			&:hover,
+			&:focus {
+				color: ${props => props.theme.colors.primary};
+			}
+		}
+	}
 
-  .stat-badges {
-    display: flex;
-    justify-content: space-between;
-    margin: 5px 0;
-    flex-wrap: wrap;
-  }
+	.stat-badges {
+		display: flex;
+		justify-content: space-between;
+		margin: 5px 0;
+		flex-wrap: wrap;
+	}
 `;
 
 const StyledCategoryBadge = styled.div`
-  margin-bottom: 5px;
-  background-color: ${props =>
-    props.color === 'white' ? props.theme.colors.textDisabled : props.color};
-  padding: 2px 4px;
-  border-radius: ${props => props.theme.sizes.borderRadius};
-  color: ${props => props.theme.colors.backgroundAccent};
-  display: flex;
-  align-items: center;
-  justify-content: center;
+	margin-bottom: 5px;
+	background-color: ${props =>
+		props.color === 'white' ? props.theme.colors.textDisabled : props.color};
+	padding: 2px 4px;
+	border-radius: ${props => props.theme.sizes.borderRadius};
+	color: ${props => props.theme.colors.backgroundAccent};
+	display: flex;
+	align-items: center;
+	justify-content: center;
 `;
 
 const TaskStats = ({ cards, dispatch, completedListId, categories }) => {
-  const deleteCard = cardId => {
-    const listId = completedListId;
+	const deleteCard = cardId => {
+		const listId = completedListId;
 
-    dispatch({
-      type: 'DELETE_CARD',
-      payload: { cardId, listId }
-    });
-  };
+		dispatch({
+			type: 'DELETE_CARD',
+			payload: { cardId, listId }
+		});
+	};
 
-  const renderCategorySummary = renderCards => {
-    const results = [];
-    let accumulated;
+	const renderCategorySummary = renderCards => {
+		const results = [];
+		let accumulated;
 
-    for (let i = 0; i < categories.length; i += 1) {
-      accumulated = renderCards
-        .filter(
-          ca => differenceInCalendarDays(ca.completedAt, new Date()) === 0
-        )
-        .filter(
-          card => card.categoryId && card.categoryId === categories[i]._id
-        )
-        .map(c => parseInt(c.minutes, 10))
-        .reduce((a, b) => a + b, 0);
+		for (let i = 0; i < categories.length; i += 1) {
+			accumulated = renderCards
+				.filter(
+					ca => differenceInCalendarDays(ca.completedAt, new Date()) === 0
+				)
+				.filter(
+					card => card.categoryId && card.categoryId === categories[i]._id
+				)
+				.map(c => parseInt(c.minutes, 10))
+				.reduce((a, b) => a + b, 0);
 
-      results.push({
-        name: [categories[i].short],
-        minutes: accumulated,
-        color: categories[i].color
-      });
-    }
+			results.push({
+				name: [categories[i].short],
+				minutes: accumulated,
+				color: categories[i].color
+			});
+		}
 
-    return results;
-  };
+		return results;
+	};
 
-  const handleStarClick = cardId => {
-    dispatch({
-      type: 'CHANGE_CARD_STAR',
-      payload: { cardId }
-    });
-  };
+	const handleStarClick = cardId => {
+		dispatch({
+			type: 'CHANGE_CARD_STAR',
+			payload: { cardId }
+		});
+	};
 
-  const renderCompletedDateSection = (renderCards, dateOffset, text) => {
-    const filteredCards = renderCards.filter(
-      c => differenceInCalendarDays(c.completedAt, new Date()) === dateOffset
-    );
+	const renderCompletedDateSection = (renderCards, dateOffset, text) => {
+		const filteredCards = renderCards.filter(
+			c => differenceInCalendarDays(c.completedAt, new Date()) === dateOffset
+		);
 
-    if (filteredCards.length !== 0) {
-      return (
-        <>
-          <h4>{text}</h4>
-          <ul>
-            {filteredCards.map(card => (
-              <li
-                key={card._id}
-                className="completed-task-wrapper"
-                style={{
-                  borderLeft: `2px solid ${
-                    card.category ? card.category.color : 'light-grey'
-                  }`
-                }}
-              >
-                <div
-                  className="completed-task-text"
-                  // eslint-disable-next-line react/no-danger
-                  dangerouslySetInnerHTML={{
-                    __html: formatMarkdown(card.text)
-                  }}
-                />
+		if (filteredCards.length !== 0) {
+			return (
+				<>
+					<h4>{text}</h4>
+					<ul>
+						{filteredCards.map(card => (
+							<li
+								key={card._id}
+								className="completed-task-wrapper"
+								style={{
+									borderLeft: `2px solid ${
+										card.category ? card.category.color : 'light-grey'
+									}`
+								}}
+							>
+								<div
+									className="completed-task-text"
+									// eslint-disable-next-line react/no-danger
+									dangerouslySetInnerHTML={{
+										__html: formatMarkdown(card.text)
+									}}
+								/>
 
-                <IconButton
-                  className="card-icon"
-                  onClick={() => deleteCard(card._id)}
-                  color="textDisabled"
-                  background="transparent"
-                >
-                  <FiXCircle />
-                </IconButton>
-              </li>
-            ))}
-          </ul>
-        </>
-      );
-    }
-    return true;
-  };
+								<IconButton
+									className="card-icon"
+									onClick={() => deleteCard(card._id)}
+									color="textDisabled"
+									background="transparent"
+								>
+									<FiXCircle />
+								</IconButton>
+							</li>
+						))}
+					</ul>
+				</>
+			);
+		}
+		return true;
+	};
 
-  const accumulated =
-    cards.length !== 0 &&
-    cards
-      .map(card => card.difficulty)
-      .reduce((accumulator, currentValue) => accumulator + currentValue);
+	const accumulated =
+		cards.length !== 0 &&
+		cards
+			.map(card => card.difficulty)
+			.reduce((accumulator, currentValue) => accumulator + currentValue);
 
-  return (
-    <TaskStatsStyled className="no-focus-mode">
-      {cards &&
-        (cards.length !== 0 && (
-          <>
-            <div className="stat-badges">
-              {renderCategorySummary(cards).map(
-                category =>
-                  !!category.minutes && (
-                    <StyledCategoryBadge
-                      key={category.name}
-                      color={category.color}
-                    >
-                      {category.minutes} min
-                    </StyledCategoryBadge>
-                  )
-              )}
-            </div>
+	return (
+		<TaskStatsStyled className="no-focus-mode">
+			{cards &&
+				(cards.length !== 0 ? (
+					<>
+						<div className="stat-badges">
+							{renderCategorySummary(cards).map(
+								category =>
+									!!category.minutes && (
+										<StyledCategoryBadge
+											key={category.name}
+											color={category.color}
+										>
+											{category.minutes} min
+										</StyledCategoryBadge>
+									)
+							)}
+						</div>
 
-            {renderCompletedDateSection(cards, 0, 'Today')}
-            {renderCompletedDateSection(cards, -1, 'Yesterday')}
-          </>
-        ))}
-    </TaskStatsStyled>
-  );
+						{renderCompletedDateSection(cards, 0, 'Today')}
+						{renderCompletedDateSection(cards, -1, 'Yesterday')}
+					</>
+				) : (
+          <div className="alert alert-message">
+            You don't have any completed tasks at this moment.
+					</div>
+				))}
+		</TaskStatsStyled>
+	);
 };
 
 TaskStats.propTypes = {
-  cards: PropTypes.arrayOf(
-    PropTypes.shape({
-      _id: PropTypes.string.isRequired,
-      text: PropTypes.string.isRequired,
-      difficulty: PropTypes.number.isRequired
-    }).isRequired
-  ),
-  completedListId: PropTypes.string.isRequired,
-  dispatch: PropTypes.func.isRequired,
-  categories: PropTypes.array.isRequired
+	cards: PropTypes.arrayOf(
+		PropTypes.shape({
+			_id: PropTypes.string.isRequired,
+			text: PropTypes.string.isRequired,
+			difficulty: PropTypes.number.isRequired
+		}).isRequired
+	),
+	completedListId: PropTypes.string.isRequired,
+	dispatch: PropTypes.func.isRequired,
+	categories: PropTypes.array.isRequired
 };
 
 export default TaskStats;
